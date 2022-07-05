fc48b1e72b95328c320227ad87db053ae26ccc58
Dan Waltin <dan@waltin.se>
2022-07-05 10:56:57 +0200

    Added empty project

---
+++ IntentsAndPermissions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IntentsAndPermissions
+//
+//  Created by Dan Waltin on 2022-07-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IntentsAndPermissions/IntentsAndPermissionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntentsAndPermissionsApp.swift
+//  IntentsAndPermissions
+//
+//  Created by Dan Waltin on 2022-07-05.
+//
+
+import SwiftUI
+
+@main
+struct IntentsAndPermissionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
